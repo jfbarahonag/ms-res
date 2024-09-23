2f824adea2aaf88e9e8f8f7e50ef12b0ef00d131
jfbarahonag <jfbarahonag@unal.edu.co>
2024-09-23 09:32:37 -0500

    updating request data schema

---
+++ app/models/maintenance.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class MaintenanceType(int, Enum):
+    TYPE_1 = 1
+    TYPE_2 = 2
+    TYPE_3 = 3
+    TYPE_4 = 4
+    TYPE_5 = 5
+    TYPE_OTHER = 6
--- app/schemas/request_schema.py
+++ app/schemas/request_schema.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from app.schemas.client_schema import ClientSchema
 from app.schemas.advisor_schema import AdvisorSchema
+from app.models.maintenance import MaintenanceType
 
 class AttachmentSchema(BaseModel):
     filename: str
@@ -10,6 +11,7 @@ class AttachmentSchema(BaseModel):
     comment: Optional[str] = "Sin comentario"
 
 class RequestData(BaseModel):
+    request_type: MaintenanceType
     attachments: Optional[list[AttachmentSchema]] = []
 
 class RequestSchema(BaseModel):
