0131751f1d8fe3e063550e9add8166cf7a7859ab
jfbarahonag <jfbarahonag@unal.edu.co>
2024-09-30 23:01:25 -0500

    validations have been added

--- app/services/reversal_service.py
+++ app/services/reversal_service.py
@@ -2,10 +2,34 @@ from fastapi import HTTPException
 from typing import Any
 
 from app.schemas.request_schema import RequestMaintenanceSchema 
-from app.schemas.maintenance_schema import MaintenanceSchema
+from app.schemas.maintenance_schema import MaintenanceSchema, AttachmentsSchema
+from app.schemas.reversal_schema import ReversalDataSchema
+
 from app.models.reversal import ReversalType
 
+from datetime import datetime, timedelta
+
+def is_valid_date(date_str):
+    try:
+        datetime.strptime(date_str, "%d/%m/%Y")
+        return True
+    except ValueError:
+        return False
+
+def is_date_more_than_N_days(date_str, N=30):
+    date_format = "%d/%m/%Y"
+    input_date = datetime.strptime(date_str, date_format)
+    today = datetime.now()
+    limit_date = today - timedelta(days=N)
+
+    return input_date < limit_date
+
+
 def validate_type(type:str, value:str):
+    if type not in ['date', 'text']:
+        return False
+    if type == 'date':
+        return is_valid_date(value)
     return True
 
 class RequestReversalService:
@@ -13,12 +37,15 @@ class RequestReversalService:
     def handle_reversal_request(data: RequestMaintenanceSchema):
         if data.maintenance.subType not in [ReversalType.porErroresCliente, ReversalType.porErroresOperativos]:
             raise HTTPException(status_code=400, detail=f"Tipo de reversion '{data.maintenance.subType}' invalido para reversiones")
+        
         reversal = RequestReversalService.parse_as_reversal(data.maintenance)
+        
+        RequestReversalService.validate_attachments(reversal, data.maintenance.attachments)
         
         return reversal
     
     @staticmethod
-    def parse_as_reversal(maintenance: MaintenanceSchema) -> dict[str, Any]:
+    def parse_as_reversal(maintenance: MaintenanceSchema) -> ReversalDataSchema:
         output = {}
         output["byOperational"] = {}
         output["byClient"] = {}
@@ -34,4 +61,20 @@ class RequestReversalService:
                     raise HTTPException(status_code=400, detail=f"Item '{item.key}' invalido")
                 output["byOperational"][item.key] = item.value
             
-        return output
+        return ReversalDataSchema(**output)
+    
+    @staticmethod
+    def validate_attachments(reversal: ReversalDataSchema, attachments: list[AttachmentsSchema]) -> ReversalDataSchema:
+        error_msg = f"Para {reversal.type.value}"
+        vobo_received = lambda pattern: any(pattern in attach.filename for attach in attachments)
+        if reversal.type == ReversalType.porErroresOperativos:
+            if not vobo_received("vobo-comercial"):
+                raise HTTPException(status_code=400, detail=f"{error_msg} el visto bueno de instancia comercial o jefe de area es obligatorio")
+        elif reversal.type == ReversalType.porErroresCliente:
+            if not vobo_received("vobo-gte-cuenta"):
+                raise HTTPException(status_code=400, detail=f"{error_msg} el visto bueno del gerente de cuenta es obligatorio")
+            
+            if  is_date_more_than_N_days(reversal.byClient.dateOfIncorrectPayment) and not vobo_received("vobo-riesgos"):
+                raise HTTPException(status_code=400, detail=f"{error_msg} el visto bueno de riesgos es obligatorio")
+            
+                
